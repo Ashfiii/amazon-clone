5c7d568e39c13fff35ecc19350f7ebbcc776100e
Ashfiii <alam19231@gmail.com>
2020-11-29 12:00:03 +0530

    Updates

---
+++ precache-manifest.88ca3e4237f7d6b81cda40d18472e9e6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b101d7311e9fc286d9c1385abbe437dd",
+    "url": "/amazon-clone/index.html"
+  },
+  {
+    "revision": "c0dc29616b49b1ef867b",
+    "url": "/amazon-clone/static/css/main.e98cffa3.chunk.css"
+  },
+  {
+    "revision": "1ef55b055f7660828ae2",
+    "url": "/amazon-clone/static/js/2.4d15201e.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/amazon-clone/static/js/2.4d15201e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0dc29616b49b1ef867b",
+    "url": "/amazon-clone/static/js/main.0f3c57b0.chunk.js"
+  },
+  {
+    "revision": "3101b3b2c4581d0f9087",
+    "url": "/amazon-clone/static/js/runtime-main.1cf4aa72.js"
+  },
+  {
+    "revision": "cb004e10772fccf16a9ec05f45acb011",
+    "url": "/amazon-clone/static/media/amazon-ad.cb004e10.jpg"
+  },
+  {
+    "revision": "b8ad586d3a9b3e5471b4850d39580d1f",
+    "url": "/amazon-clone/static/media/amazon-black.b8ad586d.png"
+  },
+  {
+    "revision": "7451f65b8ffd3f20732b3ac43e21942a",
+    "url": "/amazon-clone/static/media/amazon_logo.7451f65b.png"
+  },
+  {
+    "revision": "2feb291208482ba33d0932a9038b6578",
+    "url": "/amazon-clone/static/media/dishwasher.2feb2912.jpg"
+  },
+  {
+    "revision": "e702f5acd4cd2cc5451d73b113beb18f",
+    "url": "/amazon-clone/static/media/header-logo.e702f5ac.jpg"
+  },
+  {
+    "revision": "714941f7c91de8221f42986da45b8ba9",
+    "url": "/amazon-clone/static/media/tv.714941f7.jpg"
+  }
+]);
